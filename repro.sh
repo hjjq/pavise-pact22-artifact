90093669557b4887ba429947ddcc4c3443dc7f92
kevins981 <kevinsong981@gmail.com>
2022-08-10 19:53:40 -0400

    Fix user.mk bug and add more stuff

--- repro.sh
+++ repro.sh
@@ -9,14 +9,28 @@ popd () {
 }
 export pushd popd
 
+### Compile Pavise shared library
+echo "======================================="
+echo "Compiling Pavise shared library."
+pushd $PAVISE_ROOT
+make a_runtime_eval &> /dev/null
+if [ $? -ne 0 ]; 
+then 
+    echo "ERROR! Pavise build failed." 
+    exit 1
+fi
+echo "Pavise compilation finished successfully."
+popd 
+
 ### Preproduce Pavise ignore list results
 echo "======================================="
 echo "Preproducing Pavise ignore list results"
 # Edit PMDK user.mk to use Pavise conservative tracking pass
-printf "CC=clang CXX=clang++
+printf "CC=clang 
+CXX=clang++
 EXTRA_CFLAGS = -g -Wno-error -fexperimental-new-pass-manager -pavise=pavisenoload" > $PAVISE_ROOT/pmdk-1.10/user.mk
 # Recompile PMDK with new pass
-echo "Recompiling PMDK with new pass..."
+echo "Recompiling PMDK with new pass... (~3 min)"
 pushd $PAVISE_ROOT/pmdk-1.10
 make clean -j &> /dev/null
 make -j &> /dev/null
@@ -41,8 +55,10 @@ echo "Preproducing Pavise conservative list results"
 printf "CC=clang
 CXX=clang++
 EXTRA_CFLAGS = -g -Wno-error -fexperimental-new-pass-manager -pavise=pavisenoload_conservative" > $PAVISE_ROOT/pmdk-1.10/user.mk
+# Modify LD_LIBRARY_PATH to use PMDK without Pavise
+export LD_LIBRARY_PATH=$PAVISE_ROOT/pmdk-1.10/src/nondebug:$PAVISE_ROOT/build/lib:$PAVISE_ROOT/isa-l/lib:$PAVISE_ROOT/pmdk-1.10-no_pavise/src/examples/libpmemobj/hashmap:/usr/local/lib64:/usr/local/lib:/usr/lib/x86_64-linux-gnu
 # Recompile PMDK with new pass
-echo "Recompiling PMDK with new pass"
+echo "Recompiling PMDK with new pass... (~3 min)"
 pushd $PAVISE_ROOT/pmdk-1.10
 make clean -j &> /dev/null
 make -j &> /dev/null
@@ -69,7 +85,7 @@ EXTRA_CFLAGS = -g -Wno-error -fexperimental-new-pass-manager" > $PAVISE_ROOT/pmd
 # Modify LD_LIBRARY_PATH to use PMDK without Pavise
 export LD_LIBRARY_PATH=$PAVISE_ROOT/pmdk-1.10-no_pavise/src/nondebug:$PAVISE_ROOT/build/lib:$PAVISE_ROOT/isa-l/lib:$PAVISE_ROOT/pmdk-1.10-no_pavise/src/examples/libpmemobj/hashmap:/usr/local/lib64:/usr/local/lib:/usr/lib/x86_64-linux-gnu
 # Recompile PMDK with new pass
-echo "Recompiling PMDK with new pass" 
+echo "Recompiling PMDK with new pass... (~3 min)" 
 pushd $PAVISE_ROOT/pmdk-1.10-no_pavise
 make clean -j &> /dev/null
 make -j &> /dev/null
