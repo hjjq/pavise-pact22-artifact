ef011c03a116c4d0d2e926c095ebeff99785df74
kevins981 <kevinsong981@gmail.com>
2022-08-14 21:38:01 -0400

    update naming

--- pmdk-1.10/src/benchmarks/pact22_repro_microbench_conservative.sh
+++ pmdk-1.10/src/benchmarks/pact22_repro_microbench_conservative.sh
@@ -35,4 +35,4 @@ for bench in hashmap_atomic hashmap_tx ctree btree rbtree rtree; do
     done
 done
 
-cp ${dir_name}/${csv_name}.csv $PAVISE_ROOT/results
+cp ${dir_name}/${csv_name}.csv $PAVISE_ROOT/results/microbench_conservative.csv
